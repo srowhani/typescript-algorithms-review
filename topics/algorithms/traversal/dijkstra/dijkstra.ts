870459b1db34d99e38c5fa69040045efaef73525
Seena Rowhani <seenarowhani95@gmail.com>
2018-09-11 16:03:39 -0400

    runtime analysis on dijkstras

--- topics/algorithms/traversal/dijkstra/dijkstra.ts
+++ topics/algorithms/traversal/dijkstra/dijkstra.ts
@@ -8,6 +8,12 @@ import { adjacencyList } from "@topics/datastructures/graphs/adjacency-list";
  * 
  * Dijkstras original algorithm did not use a priority queue making it run
  * in O(|V|^2)
+ * 
+ * Runtime Complexity
+ *  Depends on the implementation of data structure used to find minimal edge to u.
+ *  https://en.wikipedia.org/wiki/Shortest_path_problem#Directed_graphs_with_nonnegative_weights
+ *  Dijkstra's algorithm with Fibonacci heap - O(E + V log V)	- Fredman & Tarjan 1984, Fredman & Tarjan 1987
+ *  Dijkstra's algorithm with list	- O(V^2) - Leyzorek et al. 1957, Dijkstra 1959
  */
 export function dijkstras(graph: { vertex: number, length: number }[][], startNode: number) {
   const resultSet = [];
